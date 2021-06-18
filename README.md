4cbafa187d26a50a830efa70fbaa1f717e5000c7
Peter <ploflan@gmail.com>
2021-06-17 22:40:20 -0400

    Basic Navbar skeleton set up

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # 7ayati
+
 ## Café and Hookah Lounge
+
+#### (7ayati is pronounced like 'hah yah ti')
 
 <br>
 
 ### General Site
+
 ### General Styling
 
 ### Navbar
-- [ ] phone nav bar hamburger menu built --- [use this as a guide](https://dev.to/andrewespejo/how-to-design-a-simple-and-beautiful-navbar-using-nextjs-and-tailwindcss-26p1)
+
+-   [ ] phone nav bar hamburger menu built --- [use this as a guide](https://dev.to/andrewespejo/how-to-design-a-simple-and-beautiful-navbar-using-nextjs-and-tailwindcss-26p1)
 
 ### Menu
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import hayatiLogo from '../public/Fayati logo no bg 200px.png';
 
 export default function Navbar() {
 	return (
-		<nav className="">
+		<nav className="flex justify-center bg-gray-500">
 			<Link href="/menu" className="">
 				<a>Menu</a>
 			</Link>
 			<Link href="/about" className="">
 				<a>About</a>
 			</Link>
-			{/* <Link href="/" className="">
-				<Image src={7ayatiLogo} alt="7ayati company logo"></Image>
-			</Link> */}
+            <Link href="/" className="">
+			    <a>
+				    <Image src={hayatiLogo} alt="7ayati company logo" />
+                </a>
+            </Link>
 			<Link href="/hours" className="">
 				<a>Hours</a>
 			</Link>
