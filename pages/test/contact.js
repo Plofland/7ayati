a509693d3d902ed391d42ac22d2d6778bf9f22a5
Plofland <ploflan@gmail.com>
2021-09-04 21:18:39 -0400

    test again

--- pages/test/contact.js
+++ pages/test/contact.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
-import styled from 'styled-components';
-import { colors, mq } from '../../themes';
-import largeParty from '../public/largeParty.jpeg';
+
 
 export default function Contact() {
 	return (
