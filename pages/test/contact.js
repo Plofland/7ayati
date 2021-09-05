14bdebc03fe1c6f24827c0963862709acf2f40a6
Plofland <ploflan@gmail.com>
2021-09-04 21:10:28 -0400

    page name change

--- pages/test/contact.js
+++ pages/test/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import styled from 'styled-components';
-import { colors, mq } from '../themes';
+import { colors, mq } from '../../themes';
 import largeParty from '../public/largeParty.jpeg';
 
 export default function Contact() {
