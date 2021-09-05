51196d202d531d81d5a97718f9d8836c32a9cb91
Plofland <ploflan@gmail.com>
2021-09-04 20:55:48 -0400

    test

--- pages/About.js
+++ pages/About.js
@@ -17,9 +17,109 @@ export default function About() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
-				<p>hello world</p>
+				<AboutSection>
+					<AboutCard>
+						<AboutText>
+							<StyledTitle>
+								About Us
+							</StyledTitle>
+							<p>
+								Lorem ipsum dolor sit amet,
+								consectetur adipiscing elit.
+								Aliquam ac pulvinar vitae
+								ipsum, lectus nisi.
+							</p>
+							<p>
+								Consectetur dui interdum
+								congue vitae ultrices.
+								Lobortis vel id facilisi
+								netus. Potenti habitasse id
+								tellus est , aenean. Laoreet
+								ac etiam pharetra faucibus
+								integer purus duis elit at.
+								Convallis semper quis
+								aliquam dis leo pellentesque
+								aliquet orci arcu. Maecenas
+								lobortis proin commodo quis
+								magna arcu senectus non
+								tincidunt. A nisl netus
+								pellentesque a consequat
+								lacus et.
+							</p>
+							<p>
+								Purus felis semper consequat
+								lectus orci, sed. Quis elit
+								ac sagittis ipsum purus
+								mauris duis tincidunt in. At
+								luctus volutpat nisl,
+								dignissim egestas.
+							</p>
+						</AboutText>
+						<ImageContainer>
+							<img
+								src={RazPortrait.src}
+								alt="Owner Portrait"
+							/>
+						</ImageContainer>
+					</AboutCard>
+				</AboutSection>
 			</main>
 		</>
 	);
 }
 
+const AboutSection = styled.div`
+	${mq({
+		margin: ['20vh auto', '20vh auto', '20vh auto']
+	})};
+	${mq({ width: ['90%', '80%', '70%'] })};
+`;
+
+const AboutCard = styled.div`
+	display: flex;
+	${mq({ flexDirection: ['column', 'column', 'row'] })};
+	align-items: center;
+	justify-content: space-between;
+	background-color: ${colors.cyanBlue};
+`;
+
+const AboutText = styled.div`
+	text-align: center;
+	${mq({ width: ['90%', '70%', '50%'] })};
+	margin: 1rem;
+
+	p {
+		padding: 1rem;
+		font-size: 0.75rem;
+	}
+`;
+
+const ImageContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	position: relative;
+	margin: 2rem 1rem;
+	${mq({ width: ['80%', '50%', '30%'] })};
+
+	img {
+		${mq({
+			position: ['static', 'static', 'absolute']
+		})};
+		${mq({
+			transform: [
+				'none',
+				'none',
+				'translate(-25%, -50%)'
+			]
+		})};
+		height: 70vh;
+	}
+`;
+
+const StyledTitle = styled.div`
+	font-family: 'Great Vibes', cursive;
+	font-size: 3rem;
+	font-weight: bold;
+	margin-left: 0;
+	text-align: center;
+`;
