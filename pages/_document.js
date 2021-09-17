975f7177e83d0b91d53e0f78b6f8674207d387be
Plofland <ploflan@gmail.com>
2021-09-17 17:26:39 -0400

    FOUC bug test

--- pages/_document.js
+++ pages/_document.js
@@ -4,36 +4,13 @@ import Document, {
 	Main,
 	NextScript
 } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(
-							<App {...props} />
-						)
-				});
-
-			const initialProps =
-				await Document.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				)
-			};
-		} finally {
-			sheet.seal();
-		}
+		const initialProps = await Document.getInitialProps(
+			ctx
+		);
+		return { ...initialProps };
 	}
 	render() {
 		return (
@@ -41,7 +18,7 @@ export default class MyDocument extends Document {
 				<Head></Head>
 				<body>
 					{/* Empty script tag as chrome bug fix, see https://stackoverflow.com/a/42969608/943337 */}
-					<script>0</script>
+                    <script>0</script>
 					<Main />
 					<NextScript />
 				</body>
