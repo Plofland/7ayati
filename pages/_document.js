1151d31f309e7b8d93195397c3cf8838b738e84c
Plofland <ploflan@gmail.com>
2021-09-17 17:43:12 -0400

    FOUC bug test

--- pages/_document.js
+++ pages/_document.js
@@ -4,39 +4,44 @@ import Document, {
 	Main,
 	NextScript
 } from 'next/document';
-
+import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
-	static async getInitialProps (ctx) {
-		const sheet = new ServerStyleSheet()
-		const originalRenderPage = ctx.renderPage
+	static async getInitialProps(ctx) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
 		try {
-		  ctx.renderPage = () =>
-			originalRenderPage({
-			  enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-			})
-			
-		  const initialProps = await Document.getInitialProps(ctx)
-		  return {
-			...initialProps,
-			styles: (
-			  <>
-				{initialProps.styles}
-				{sheet.getStyleElement()}
-			  </>
-			)
-		  }
+			ctx.renderPage = () =>
+				originalRenderPage({
+					//eslint-disable-next-line
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(
+							<App {...props} />
+						)
+				});
+
+			const initialProps =
+				await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				)
+			};
 		} finally {
-		  sheet.seal()
+			sheet.seal();
 		}
-	  }
+	}
 	render() {
 		return (
 			<Html lang="en">
 				<Head></Head>
 				<body>
 					{/* Empty script tag as chrome bug fix, see https://stackoverflow.com/a/42969608/943337 */}
-                    <script>0</script>
+					<script>0</script>
 					<Main />
 					<NextScript />
 				</body>
